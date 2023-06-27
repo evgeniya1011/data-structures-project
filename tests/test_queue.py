fca8dc76980088331d2f70228e986f8e3383b2c5
evgeniya1011 <zhdavydova@mail.ru>
2023-06-28 00:40:35 +0500

    add test for dequeue

--- tests/test_queue.py
+++ tests/test_queue.py
@@ -25,3 +25,11 @@ class TestQueue(unittest.TestCase):
         queue.enqueue("data1")
         queue.enqueue("data2")
         self.assertEqual(str(queue), "data1\ndata2")
+
+    def test_dequeue(self):
+        queue = Queue()
+        queue.enqueue("data1")
+        queue.enqueue("data2")
+        self.assertEqual(queue.dequeue(), "data1")
+        self.assertEqual(queue.dequeue(), "data2")
+        self.assertEqual(queue.dequeue(), None)
