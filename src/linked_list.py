145ca87af7efe219878f92531abc771cc67b1687
evgeniya1011 <zhdavydova@mail.ru>
2023-07-02 22:06:44 +0500

    add class linkedlist

--- src/linked_list.py
+++ src/linked_list.py
@@ -1,18 +1,36 @@
 class Node:
     """Класс для узла односвязного списка"""
-    pass
+    def __init__(self, data: object, next_node=None):
+        """
+        Конструктор класса Node
+
+        :param data: данные, которые будут храниться в узле
+        """
+        self.data = data
+        self.next_node = next_node
 
 
 class LinkedList:
     """Класс для односвязного списка"""
 
+    def __init__(self):
+        self.head = None
+
     def insert_beginning(self, data: dict) -> None:
         """Принимает данные (словарь) и добавляет узел с этими данными в начало связанного списка"""
-        pass
+        new_node = Node(data, None)
+        new_node.next_node = self.head
+        self.head = new_node
 
     def insert_at_end(self, data: dict) -> None:
         """Принимает данные (словарь) и добавляет узел с этими данными в конец связанного списка"""
-        pass
+        new_node = Node(data, None)
+        if self.head is None:
+            self.head = new_node
+        tail = self.head
+        while tail.next_node:
+            tail = tail.next_node
+        tail.next_node = new_node
 
     def __str__(self) -> str:
         """Вывод данных односвязного списка в строковом представлении"""
