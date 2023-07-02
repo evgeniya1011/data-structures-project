14725f7dd2e24f43b11a90debc62802b49860858
evgeniya1011 <zhdavydova@mail.ru>
2023-07-02 22:07:33 +0500

    add tests for linkedlist

--- tests/test_linked_list.py
+++ tests/test_linked_list.py
@@ -1,1 +1,35 @@
 """Здесь надо написать тесты с использованием unittest для модуля linked_list."""
+import unittest
+from src.linked_list import Node, LinkedList
+
+
+class TestNode(unittest.TestCase):
+
+    def test_init(self):
+        n1 = Node(7)
+        self.assertEqual(n1.data, 7)
+        self.assertEqual(n1.next_node, None)
+
+
+class TestLinkedList(unittest.TestCase):
+
+    def test_insert_beginning(self):
+        ll = LinkedList()
+        ll.insert_beginning({'id': 1})
+        ll.insert_beginning({'id': 0})
+        self.assertEqual(str(ll), " {'id': 0} -> {'id': 1} -> None")
+
+    def test_insert_at_end(self):
+        ll = LinkedList()
+        ll.insert_beginning({'id': 1})
+        ll.insert_at_end({'id': 2})
+        ll.insert_at_end({'id': 3})
+        self.assertEqual(str(ll), " {'id': 1} -> {'id': 2} -> {'id': 3} -> None")
+
+    def test_str(self):
+        ll = LinkedList()
+        ll.insert_beginning({'id': 1})
+        ll.insert_at_end({'id': 2})
+        ll.insert_at_end({'id': 3})
+        ll.insert_beginning({'id': 0})
+        self.assertEqual(str(ll), " {'id': 0} -> {'id': 1} -> {'id': 2} -> {'id': 3} -> None")
